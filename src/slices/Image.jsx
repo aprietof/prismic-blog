b26c7a0b7ffa1c0f65342f93e00e3be4aa796a93
Adrian Prieto <adrianprieto00@gmail.com>
2018-11-18 15:20:05 -0500

    Basic blog setup

---
+++ src/slices/Image.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+import Img from 'gatsby-image';
+
+const Content = styled.div`
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+`;
+
+const Image = ({ input }) => (
+  <Content>
+    <Img fluid={input.primary.image.localFile.childImageSharp.fluid} />
+    <small>{input.primary.caption}</small>
+  </Content>
+);
+
+export default Image;
+
+Image.propTypes = {
+  input: PropTypes.object.isRequired,
+};

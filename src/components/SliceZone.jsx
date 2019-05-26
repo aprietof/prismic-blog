a93cfae5f82e834c7f6ca407f5b98fcfaa73dc73
Adrian Prieto <adrianprieto00@gmail.com>
2019-05-26 13:30:23 -0400

    Update body font styles

--- src/components/SliceZone.jsx
+++ src/components/SliceZone.jsx
@@ -12,8 +12,9 @@ const Content = styled.div`
     letter-spacing: -0.003em;
     --baseline-multiplier: 0.179;
     --x-height-multiplier: 0.35;
-    font-size: 21px;
-    line-height: 1.58;
+    font-size: 19px;
+    line-height: 1.68;
+
     code {
       padding: 0.2rem 0.5rem;
       margin: 0.5rem 0;

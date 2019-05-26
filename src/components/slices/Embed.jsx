c6b8c9c1683fdc4b4990a3d1a43cbbf7e2b03fa1
Adrian Prieto <adrianprieto00@gmail.com>
2019-05-26 19:56:38 -0400

    Add support for tweet embeds

--- src/components/slices/Embed.jsx
+++ src/components/slices/Embed.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import getVideoId from 'get-video-id';
+import { Tweet } from 'react-twitter-widgets';
+import getTweetId from 'twitter-url-parser';
 import ResponsiveIframe from '../ResponsiveIframe';
 
 export default function Embed({ input }) {
   const {
     primary: {
-      content: { provider_name: providerName, title, embed_url: url },
+      content: { provider_name: providerName, title, embed_url: url, html },
     },
   } = input;
 
@@ -22,13 +24,15 @@ export default function Embed({ input }) {
                 title={title}
               />
             );
+          case 'Twitter':
+            return (
+              <IframeWrapper>
+                <Tweet tweetId={getTweetId(url).id} />
+              </IframeWrapper>
+            );
 
           default:
-            return (
-              <p>
-                {providerName} : {url}
-              </p>
-            );
+            return <DefaultEmbed dangerouslySetInnerHTML={{ __html: html }} />;
         }
       })()}
     </Content>
@@ -41,6 +45,16 @@ Embed.propTypes = {
 
 const Content = styled.div`
   max-width: ${props => props.theme.maxWidthText};
+  margin: ${props => `0 auto ${props.theme.contentSeparator}`};
+`;
+
+const IframeWrapper = styled.div`
   margin: ${props => props.theme.margin.center};
-  padding-bottom: ${props => `calc(${props.theme.contentSeparator} / 4)`};
+  max-width: 500px;
+`;
+
+const DefaultEmbed = styled.div`
+  display: block;
+  margin: ${props => props.theme.margin.center};
+  max-width: 100%;
 `;

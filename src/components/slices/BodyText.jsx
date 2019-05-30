4f8b2835fec90928f452618adb733e086b27d9f5
Adrian Prieto <adrianprieto00@gmail.com>
2019-05-30 19:43:42 -0400

    Update body text slice styles

--- src/components/slices/BodyText.jsx
+++ src/components/slices/BodyText.jsx
@@ -5,6 +5,14 @@ import styled from 'react-emotion';
 const Content = styled.div`
   max-width: ${props => props.theme.maxWidthText};
   margin: ${props => props.theme.margin.center};
+
+  p {
+    font-size: 17px;
+    color: ${props => props.theme.colors.greyBlueDark};
+    letter-spacing: 0.2px;
+    line-height: 1.8;
+    margin-bottom: ${props => props.theme.contentSeparator};
+  }
 `;
 
 const BodyText = ({ input }) => <Content dangerouslySetInnerHTML={{ __html: input.primary.text.html }} />;

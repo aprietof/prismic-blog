d0c1b7379bf276716eb7f0b2344d8a1879bd8b5c
Adrian Prieto <adrianprieto00@gmail.com>
2019-06-02 10:00:00 -0400

    Update theme attributes

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,17 @@
 const theme = {
   colors: {
-    primary: '#3D63AE',
+    primary: '#2773FF',
     bg: '#fff',
     black: '#000',
     greyLight: '#EBEDF2',
+    greyLighter: 'rgba(0,0,0,.54)',
     greyBlue: '#a2bce2',
+    greyBlueDark: '#434456',
+    greyBlueDarker: '#2e2f42',
     grey: '#595C62',
     greyDark: '#303643',
     greyDarker: '#1c252b',
+    white: '#FFFFFF',
   },
   maxWidth: '1000px',
   maxWidthText: '720px',
@@ -23,7 +27,7 @@ const theme = {
     },
     center: '0 auto',
   },
-  contentSeparator: '1.45rem',
+  contentSeparator: '2.45rem',
 };
 
 export default theme;
